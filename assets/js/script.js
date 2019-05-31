f1b2f1a9e668699e3d7b4b473203d0a9bffdfb8e
Saurik <49932576+lolzadopted@users.noreply.github.com>
2019-05-31 13:25:39 -0700

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+var app = new Vue({
+  	el: '#app',
+  	data: {
+		info: {
+      name: "adopted.",
+      social: [
+        {
+          link: "https://twitter.com/lolzadopted",
+          type: "twitter",
+          name: "Twitter"
+        },
+        {
+          link: "https://discord.gg/KYxPnFG",
+          type: "discord",
+          name: "Discord"
+        },
+        {
+          link: "https://reddit.com/u/ItsS0n1c",
+          type: "reddit",
+          name: "Reddit"
+        },
+        {
+          link: "https://github.com/lolzadopted",
+          type: "github",
+          name: "Github"
+        },
+        {
+          link: "mailto:lolzadopted@gmail.com",
+          type: "mail",
+          icon_overwrite: {
+            type: "fas",
+            id: "envelope"
+          },
+          name: "Mail"
+        }
+
+      ],
+      projects: {}
+    }
+  	},
+  	methods: {
+		getEndpoint(url) {
+	 		return new Promise((resolve, reject) => {
+				fetch(`${url}`).then((response) => {
+					if (response.status !== 200) {
+		  				console.log(`Looks like there was a problem. Status Code: ${response.status}`);
+						reject(response.status);
+		  				return;
+					};
+					// Examine the text in the response
+					response.json().then((data) => resolve(data));
+				}).catch((err) => reject(err));
+	  		});
+		},
+		init() {
+			this.getEndpoint("assets/json/projects.json").then((data) => {
+				this.info.projects = data;
+	  		}).catch((err) => {
+	  			console.error(err);
+	  		});
+		}
+  	}
+});
+
+app.init();
