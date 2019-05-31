d0a8f4bbf18579c9e54792d433c0b7efdfd791ca
lolzadopted <lolzadopted@gmail.com>
2019-05-31 15:14:13 -0700

    Website Reorganization

--- assets/website/error.js
+++
@@ -1,8 +0,0 @@
-document.documentElement.classList.add("js");
-
-document.addEventListener("DOMContentLoaded", function() {
-	document.querySelector("#button-details").addEventListener("click", function(e) {
-		e.preventDefault();
-		document.body.classList.add("show-details");
-	});
-});
